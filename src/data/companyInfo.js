e27dd40ff24c3de653974cb566468f082f53458d
ankitkumargiri11082006 <ankitgoswami.svu@gmail.com>
2026-03-14 22:04:06 +0530

    Number Update

--- src/data/companyInfo.js
+++ src/data/companyInfo.js
@@ -3,8 +3,8 @@ export const companyInfo = {
   tagline: 'Technology partner for web, cloud, data, and AI solutions',
   email: 'indocreonix@gmail.com',
   careersEmail: 'indocreonix@gmail.com',
-  phones: ['9667097901', '7827121924'],
-  primaryPhone: '9667097901',
+  phones: ['7827456620', '9667097901'],
+  primaryPhone: '7827456620',
   addressLines: [
     'PLOT NO. 340/A, KH.NO. 4/7, GROUND FLOOR, BLK-C, RAJEEV NAGAR,',
     'VILLAGE BEGAMPUR, CITY DELHI-110086',
